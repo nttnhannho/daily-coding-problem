c2dd8bece82c4325a80691dbbaa8f968d9139cd9
nttnhannho <makelifeperfect@gmail.com>
2021-09-19 19:57:36 +0700

    Add solution for problem 01

---
+++ problem_01.py
@@ -0,0 +1,26 @@
+"""
+[Easy]
+
+This problem was recently asked by Google.
+
+Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+Bonus: Can you do this in one pass?
+"""
+
+
+def is_found(lst, k):
+    for index, value in enumerate(lst):
+        temp = k - value
+        if temp in lst[index + 1:]:
+            return True
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    sample_lst = [10, 15, 3, 7]
+    for sample_k in range(30):
+        print(f"{sample_k} : {is_found(sample_lst, sample_k)}")
