eb61fd838d1aff466722a193001c219905587370
nttnhannho <makelifeperfect@gmail.com>
2021-12-28 00:55:12 +0700

    Add solution for problem 07

---
+++ problem_07.py
@@ -0,0 +1,34 @@
+"""
+[Medium]
+
+This problem was asked by Facebook.
+
+Given the mapping a = 1, b = 2, ... z = 26, and an encoded message, count the number of ways it can be decoded.
+
+For example, the message '111' would give 3, since it could be decoded as 'aaa', 'ka', and 'ak'.
+
+You can assume that the messages are decodable. For example, '001' is not allowed.
+"""
+from collections import defaultdict
+
+
+def solution(s):
+    cache = defaultdict(int)
+    cache[len(s)] = 1
+
+    for i in reversed(range(len(s))):
+        if s[i].startswith('0'):
+            cache[i] = 0
+        elif i == len(s) - 1:
+            cache[i] = 1
+        else:
+            if int(s[i:i + 2]) <= 26:
+                cache[i] = cache[i + 2]
+            cache[i] += cache[i + 1]
+    return cache[0]
+
+
+if __name__ == "__main__":
+    print(solution("111"))
+    print(solution("011"))
+    print(solution("602"))
