e8914d98206fcfa53dcdd11a2ddc1a3b13576775
nttnhannho <makelifeperfect@gmail.com>
2021-12-28 20:14:16 +0700

    Add leetcode solution for problem 242

---
+++ leetcode/problem_242.py
@@ -0,0 +1,31 @@
+"""
+[Easy]
+
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+Example 1:
+Input: s = "anagram", t = "nagaram"
+Output: true
+
+Example 2:
+Input: s = "rat", t = "car"
+Output: false
+
+Constraints:
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
+
+Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+"""
+from collections import Counter
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(s) == Counter(t)
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.isAnagram("anagram", "nagaram"))
+    print(solution.isAnagram("rat", "car"))
